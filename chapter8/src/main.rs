d6b676b474dd37203943f6b222fc4018ec344524
Yuya Minami <yuya373@me.com>
2018-07-19 17:03:18 +0900

    chapter8 hashmap

--- chapter8/src/main.rs
+++ chapter8/src/main.rs
@@ -54,4 +54,180 @@ fn main() {
         SpreadsheetCell::Float(3.0),
     ];
     println!("row: {:?}", row);
+
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+    println!("scores: {:?}", scores);
+
+    scores.insert(String::from("Blue"), 99);
+    println!("scores: {:?}", scores);
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+    println!("scores: {:?}", scores);
+
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", map);
+
+    // let mut count = 0;
+    // count += 1;
+    // println!("count: {:?}", count);
+
+    // let mut count = &0;
+    // count += 1;
+    // println!("count: {:?}", count);
+
+    let l = vec![1, 2, 3, 4, 5];
+    println!("{:?} -> average: {:?}", l, average(&l));
+    let l = vec![1, 2, 3, 4, 5];
+    println!("{:?} -> median: {:?}", l, median(&l));
+    let l = vec![1, 2, 3, 4, 5, 6];
+    println!("{:?} -> median: {:?}", l, median(&l));
+    let l = vec![1, 2, 3, 4, 5, 3];
+    println!("{:?} -> mode: {:?}", l, mode(&l));
+
+    println!("apple -> {:?}", str_to_pig_latin("apple"));
+    println!("first -> {:?}", str_to_pig_latin("first"));
+
+    company::main();
+}
+
+fn average(l: &Vec<i32>) -> f32 {
+    let mut sum = 0.0;
+
+    for i in l.into_iter() {
+        sum += *i as f32;
+    }
+
+    return sum / l.len() as f32;
+}
+
+fn median(l: &Vec<i32>) -> f32 {
+    if l.len() % 2 == 0 {
+        let middle = l.len() / 2;
+        let ll = &l[middle - 1..middle + 1];
+        let sum = ll[0] + ll[1];
+        return sum as f32 / 2.0;
+    } else {
+        return l[l.len() / 2] as f32;
+    }
+}
+
+fn mode(l: &Vec<i32>) -> i32 {
+    use std::collections::HashMap;
+
+    let mut map = HashMap::new();
+
+    for i in l.into_iter() {
+        let mut count = map.entry(i).or_insert(0);
+        *count += 1;
+    }
+
+    let (mode, _) = map.iter()
+        .max_by(|&x, &y| {
+            let (_, v1) = x;
+            let (_, v2) = y;
+            return v1.cmp(v2);
+        })
+        .unwrap();
+
+    return **mode;
+}
+
+fn str_to_pig_latin(s: &str) -> Option<String> {
+    let mut chars = s.chars();
+
+    match chars.next() {
+        Some(a) => if a == 'a' {
+            return Some(format!("{:}{:}-hay", a, chars.as_str()));
+        } else {
+            return Some(format!("{:}-{:}ay", chars.as_str(), a));
+        },
+        None => None,
+    }
+}
+
+mod company {
+    use std::collections::HashMap;
+    type Dic = HashMap<String, Vec<String>>;
+
+    pub fn main() {
+        let mut dic = HashMap::new();
+        run("Add a to 1", &mut dic);
+        run("Add b to 2", &mut dic);
+        run("Add c to 1", &mut dic);
+        print_members("1", &dic);
+        print_all_members(&dic);
+    }
+
+    fn run(s: &str, dic: &mut Dic) {
+        let mut itr = s.split_whitespace();
+        let verb = itr.next();
+
+        match verb {
+            Some(a) => if a == "Add" {
+                let name = itr.next();
+                let _ = itr.next();
+                let department = itr.next();
+                match department {
+                    Some(d) => match name {
+                        Some(n) => {
+                            let members = dic.entry(String::from(d)).or_insert(vec![]);
+                            members.push(String::from(n));
+                        }
+                        None => {}
+                    },
+                    None => {}
+                }
+            } else {
+            },
+            None => {}
+        }
+    }
+
+    fn print_members(dep: &str, dic: &Dic) {
+        match dic.get(dep) {
+            Some(members) => {
+                println!("In {:?} department: ", dep);
+                let mut members = members.clone();
+                &members.sort();
+                for member in members.iter() {
+                    println!("{:?}", member);
+                }
+            }
+            None => println!("No members in {:?}", dep),
+        }
+    }
+
+    fn print_all_members(dic: &Dic) {
+        let mut all_members: Vec<(&String, &String)> = vec![];
+
+        for (dep, members) in dic.iter() {
+            for member in members.iter() {
+                all_members.push((member, dep));
+            }
+        }
+
+        &all_members.sort_by(|&a, &b| {
+            let (name1, _) = a;
+            let (name2, _) = b;
+            return name1.cmp(name2);
+        });
+
+        for &(name, dept) in all_members.iter() {
+            println!("{:?}: {:?}", name, dept);
+        }
+    }
 }
