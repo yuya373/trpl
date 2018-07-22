0acd7119fb83394a75a14828827243875e72341f
Yuya Minami <yuya373@me.com>
2018-07-22 21:28:53 +0900

    Removing Duplication by Extracting a Function

--- chapter10/src/main.rs
+++ chapter10/src/main.rs
@@ -1,3 +1,24 @@
+// parameter: slice of i32
+fn largest(l: &[i32]) -> i32 {
+    let mut largest: i32 = l[0];
+
+    // why `iter` ?
+    for &n in l.iter() {
+        if n > largest {
+            largest = n;
+        }
+    }
+
+    largest
+}
+
 fn main() {
-    println!("Hello, world!");
+    let number_list = vec![35, 50, 25, 100, 65];
+    let result = largest(&number_list);
+
+    println!("The largest number is {}", result);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 }
