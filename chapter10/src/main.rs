204b4de7c090b7c5b9d9056cc37e0adb0b7ef08b
Yuya Minami <yuya373@me.com>
2018-07-23 00:33:36 +0900

    add largest_char

--- chapter10/src/main.rs
+++ chapter10/src/main.rs
@@ -1,5 +1,5 @@
 // parameter: slice of i32
-fn largest(l: &[i32]) -> i32 {
+fn largest_i32(l: &[i32]) -> i32 {
     let mut largest: i32 = l[0];
 
     // why `iter` ?
@@ -12,13 +12,31 @@ fn largest(l: &[i32]) -> i32 {
     largest
 }
 
+fn largest_char(l: &[char]) -> char {
+    let mut largest = l[0];
+
+    for &n in l {
+        if n > largest {
+            largest = n;
+        }
+    }
+
+    largest
+}
+
 fn main() {
     let number_list = vec![35, 50, 25, 100, 65];
-    let result = largest(&number_list);
-
+    let result = largest_i32(&number_list);
     println!("The largest number is {}", result);
+    assert_eq!(result, 100);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largest(&number_list);
+    let result = largest_i32(&number_list);
     println!("The largest number is {}", result);
+    assert_eq!(result, 6000);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
+    assert_eq!(result, 'y');
 }
