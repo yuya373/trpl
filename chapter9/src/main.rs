1a132b160cee4c3a94e50bff9706d29fdfba6c3d
Yuya Minami <yuya373@me.com>
2018-07-22 18:19:20 +0900

    ensure value is in valid range

--- chapter9/src/main.rs
+++ chapter9/src/main.rs
@@ -1,8 +1,33 @@
-use std::net::IpAddr;
+#[derive(Debug)]
+pub struct Guess {
+    // It’s important that the value field be private so code using the Guess struct is not allowed to set value directly
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        // test `value`
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+        Guess { value }
+    }
+
+    // getter
+    pub fn value(&self) -> u32 {
+        self.value
+    }
+}
+
+fn important_func(guess: &Guess) {
+    // do something important with value...
+    println!("Guess: {:?}", guess);
+}
 
 fn main() {
-    // 127.0.0.1 is always valid ip address
-    // it is acceptable to use `unwarp`
-    let home: IpAddr = "127.0.0.1".parse().unwrap();
-    println!("home: {:?}", home);
+    // panic!
+    // let guess = Guess::new(1000);
+    // important_func(&guess);
+    let guess = Guess::new(1);
+    important_func(&guess);
 }
