fb735024d53b23913b7388f6ad8c64f53c1598a7
Yuya Minami <yuya373@me.com>
2018-07-23 00:46:06 +0900

    generic largest (doesn't compile yet)

--- chapter10/src/main.rs
+++ chapter10/src/main.rs
@@ -24,19 +24,33 @@ fn largest_char(l: &[char]) -> char {
     largest
 }
 
+// parameter `l` is slice of values of type T
+// `largest` return a value of type T
+fn largest<T>(l: &[T]) -> T {
+    let mut largest: T = l[0];
+
+    for &n in l {
+        if n > largest {
+            largest = n;
+        }
+    }
+
+    largest
+}
+
 fn main() {
     let number_list = vec![35, 50, 25, 100, 65];
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
     assert_eq!(result, 100);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
     assert_eq!(result, 6000);
 
     let char_list = vec!['y', 'm', 'a', 'q'];
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("The largest char is {}", result);
     assert_eq!(result, 'y');
 }
