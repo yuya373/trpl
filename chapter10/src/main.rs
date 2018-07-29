c043f1d0b9c9efc812469cbb7042c9ff9edb00a0
Yuya Minami <yuya373@me.com>
2018-07-29 21:25:43 +0900

    struct method definition

--- chapter10/src/main.rs
+++ chapter10/src/main.rs
@@ -1,25 +1,40 @@
 #[derive(Debug)]
-enum Option<T> {
-    Some(T),
-    None,
+struct Point<T, U> {
+    x: T,
+    y: U,
 }
 
-#[derive(Debug)]
-enum Result<T, E> {
-    Ok(T),
-    Err(E),
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+impl Point<f32, f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
 }
 
 fn main() {
-    let some: Option<i64> = Option::Some(3);
-    println!("some: {:?}", some);
+    let p = Point { x: 3, y: 10 };
+    println!("p.x = {}", p.x()); // p.x is not equal to p.x();
 
-    let none: Option<String> = Option::None;
-    println!("none: {:?}", none);
+    // p.distance_from_origin(); // only Point<f32> have this method
 
-    let ok: Result<i64, String> = Result::Ok(3);
-    println!("ok: {:?}", ok);
+    let p = Point { x: 3.0, y: 10.0 };
+    println!("distance from origin: {:?}", p.distance_from_origin());
 
-    let err: Result<i64, String> = Result::Err(String::from("hoge"));
-    println!("err: {:?}", err);
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+    let p3 = p1.mixup(p2);
+    // println!("p1: {:?}", p1); // p1 and p2 is moved value
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
