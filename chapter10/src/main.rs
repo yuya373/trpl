0470f72a74cf835dc5a00d6932b081b54f475c81
Yuya Minami <yuya373@me.com>
2018-08-04 22:33:16 +0900

    string2 doesnt live long enough

--- chapter10/src/main.rs
+++ chapter10/src/main.rs
@@ -7,9 +7,10 @@ fn longest<'a>(a: &'a str, b: &'a str) -> &'a str {
 }
 fn main() {
     let string1 = String::from("long string is long");
+    let result;
     {
         let string2 = String::from("xyz");
-        let result = longest(string1.as_str(), string2.as_str());
-        println!("The longest string is `{}`", result);
+        result = longest(string1.as_str(), string2.as_str());
     }
+    println!("The longest string is `{}`", result);
 }
