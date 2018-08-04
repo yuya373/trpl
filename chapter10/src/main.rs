ef24274be752dd229db4b74e91d82f73f6eefa95
Yuya Minami <yuya373@me.com>
2018-08-05 00:47:08 +0900

    dangling reference (does not compile)

--- chapter10/src/main.rs
+++ chapter10/src/main.rs
@@ -1,16 +1,5 @@
-fn longest<'a>(a: &'a str, b: &'a str) -> &'a str {
-    if a.len() > b.len() {
-        a
-    } else {
-        b
-    }
-}
-fn main() {
-    let string1 = String::from("long string is long");
-    let string2 = String::from("xyz");
-    let result;
-    {
-        result = longest(string1.as_str(), string2.as_str());
-    }
-    println!("The longest string is `{}`", result);
-}
+fn longest<'a>(x: &str, y: &str) -> &'a str {
+    let result = String::from("really long string");
+    result.as_str()
+} // result drops here.
+fn main() {}
