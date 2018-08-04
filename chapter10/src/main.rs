0ecae76b66e583b809fad84fc81702e7d8c0297e
Yuya Minami <yuya373@me.com>
2018-08-04 22:34:05 +0900

    move string2 to outer scope (does not compile)

--- chapter10/src/main.rs
+++ chapter10/src/main.rs
@@ -7,9 +7,9 @@ fn longest<'a>(a: &'a str, b: &'a str) -> &'a str {
 }
 fn main() {
     let string1 = String::from("long string is long");
+    let string2 = String::from("xyz");
     let result;
     {
-        let string2 = String::from("xyz");
         result = longest(string1.as_str(), string2.as_str());
     }
     println!("The longest string is `{}`", result);
