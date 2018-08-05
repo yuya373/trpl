077f98d8847e8ef3a042d3a9db09338da4ddcc90
Yuya Minami <yuya373@me.com>
2018-08-05 15:13:20 +0900

    lifetime for struct

--- chapter10/src/main.rs
+++ chapter10/src/main.rs
@@ -1,5 +1,22 @@
-fn longest<'a>(x: &str, y: &str) -> &'a str {
-    let result = String::from("really long string");
-    result.as_str()
-} // result drops here.
-fn main() {}
+#[derive(Debug)]
+struct ImportantExcept<'a> {
+    part: &'a str,
+}
+
+fn main() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcept {
+        part: first_sentence,
+    };
+    println!("i: {:?}", i);
+
+    let e;
+    {
+        let s = String::from("str");
+        // s does not live long enough
+        let t = s.as_str();
+        e = ImportantExcept { part: t };
+    }
+    println!("e: {:?}", e);
+}
