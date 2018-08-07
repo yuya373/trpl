a44a0c09f12646a37cdfc1d60dcf404910d06f75
Yuya Minami <yuya373@me.com>
2018-08-08 00:15:16 +0900

    three elision rules

--- chapter10/src/main.rs
+++ chapter10/src/main.rs
@@ -1,12 +1,17 @@
+// The first rule is that each parameter that is a reference gets its own lifetime parameter.
+// The second rule is if there is exactly one input lifetime parameter, that lifetime is assigned to all output lifetime parameters
+// The third rule is if there are multiple input lifetime parameters, but one of them is &self or &mut self because this is a method, the lifetime of self is assigned to all output lifetime parameters.
 struct ImportantExcerpt<'a> {
     part: &'a str,
 }
 
 impl<'a> ImportantExcerpt<'a> {
+    // first lifetime elision rule
     fn level(&self) -> i32 {
         3
     }
 
+    // third lifetime elision rule
     fn announce_and_return_part(&self, announcement: &str) -> &str {
         println!("Attention please: {}", announcement);
         self.part
